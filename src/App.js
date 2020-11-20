8561889ae3bf0ae5ab9cf1719b870dfae58c938d
Jesica Ababon <jesababon@gmail.com>
2020-11-20 00:23:30 -0500

    Got cms css working, file needed to be copied. React would not read via node module.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
-import './App.css';
+import './CMS.css';
+// import Button from '@cmsgov/design-system/dist/components/Button/Button';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header ds-u-fill--primary">
+        <img src={logo} className="App-logo " alt="logo" />
+        </header>
+        <p className="ds-display">
+          <button type="button" className="ds-c-button ds-c-button--primary ds-c-button--hover ds-c-button--big">
+            Stuff
+          </button>
+        </p>
         <p>
+          <button className="ds-c-button ds-c-button--danger">
+          Right icon<svg className="ds-u-margin-left--0" aria-hidden="true" viewBox="0 0 24 24">
+          </svg>
+          {/* <use xlink:href=''></use> */}
+        </button>
+        </p>
+        <p className="ds-title">
           Now we're rocking.
         </p>
+        <ul className="ds-c-list ds-u-font-size--h3 " >
+          <li><strong>Legislative</strong> – Makes laws (Congress)</li>
+          <li><strong>Executive</strong> – Carries out laws (President, Vice President, Cabinet)</li>
+          <li><strong>Judicial</strong> – Evaluates laws (Supreme Court and other courts)</li>
+        </ul>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="ds-c-link--inverse"
+          href="https://www.linkedin.com/in/jesababon/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Connect with Jes
         </a>
-      </header>
     </div>
   );
 }
