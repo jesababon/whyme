8a90d364695f77b63500db2a0d06f76fc388e785
Jesica Ababon <jesababon@gmail.com>
2020-11-20 18:04:43 -0500

    multi-checkbox need to map through all for state update

--- src/2page.js
+++ src/2page.js
@@ -1,57 +1,80 @@
 import React from 'react'
 import {ReactComponent as RightArrow} from './images/next.svg';
 import ChoiceList from '@cmsgov/design-system/dist/components/ChoiceList/ChoiceList'
-import Choice from '@cmsgov/design-system/dist/components/ChoiceList/Choice'
+// import Choice from '@cmsgov/design-system/dist/components/ChoiceList/Choice'
 
-function Second(){
-return(
-   <div>
-     <container>
-      <p className="ds-title">
-          What qualities are you looking for in a junior dev?
-      </p>
-      <br/>
-      <fieldset className="ds-c-fieldset ds-u-display--inline-block">
-      <ChoiceList
-         choices={[
-         { label: 'Believes in clean code and follows OOP.', value: 'A', defaultChecked:true},
-         { label: 'Strong passion for good design.', value: 'B' },
-         { label: 'Eager to learn whatever is required.', value: 'B' },
-         { label: 'Strong interest in civic tech.', value: 'B' },
-         { label: 'Experience being a team player.', value: 'B' },
-         { label: 'Strong interest in civic tech', value: 'B' },
-         ]}
-         className=" checkbox_choices"
-         label="Choose your requirements."
-         hint="Minimum of one requirement."
-         type="checkbox"
-      />
-      <Choice
-      //   defaultChecked
-        className="checkbox_choices"
-        type="checkbox"
-        label="Loves dogs."
-        value="b"
-        checkedChildren={<div className="ds-c-choice__checkedChild"
-        >something</div>}
-      />
-      </fieldset>
-      <br/>
-        {/* <input type="text" className="ds-c-field ds-u-display--inline-block" /> */}
-        &nbsp;
-        <button className="ds-c-button ds-c-button--primary ds-u-margin-top--2">
-          Submit
-          <RightArrow className="ds-u-margin-left--1"/>
-        </button>
-        <br/>
-        {/* <ul className="ds-c-list ds-u-font-size--h5 " >
-          <li><strong>Legislative</strong> – Makes laws (Congress)</li>
-          <li><strong>Executive</strong> – Carries out laws (President, Vice President, Cabinet)</li>
-          <li><strong>Judicial</strong> – Evaluates laws (Supreme Court and other courts)</li>
-        </ul> */}
-      </container>
-   </div>
-)
+class Second extends React.Component{
+   constructor(props){
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+
+      console.log(this);
+     
+
+    }
+    render(){
+      return(
+         <div>
+            <p className="ds-title">
+               What qualities are you looking for in a junior dev?
+            </p>
+            <br/>
+            <fieldset className="ds-c-fieldset ds-u-display--inline-block" onChange={this.handleChange}>
+            <ChoiceList
+               choices={[
+               { label: 'Believes in clean code and follows OOP.', value: 'A', defaultChecked:true},
+               { label: 'Strong passion for good design.', value: 'B' },
+               { label: 'Eager to learn whatever is required.', value: 'C' },
+               { label: 'Strong interest in civic tech.', value: 'D' },
+               { label: 'Experience being a team player.', value: 'E' },
+               { label: 'Strong interest in civic tech', value: 'F' },
+               ]}
+               className=" checkbox_choices"
+               name=" checkbox_choices"
+               label="Choose your requirements."
+               hint="Minimum of one requirement."
+               type="checkbox"
+            />
+            {/* <Choice
+            //   defaultChecked
+            className="checkbox_choice"
+            name="checkbox_choice"
+            type="checkbox"
+            label="Loves dogs."
+            value="G"
+            checkedChildren={<div className="ds-c-choice__checkedChild"
+            >something</div>}
+            /> */}
+            </fieldset>
+            <br/>
+            {/* <input type="text" className="ds-c-field ds-u-display--inline-block" /> */}
+            &nbsp;
+            <button onClick={this.handleSubmit} className="ds-c-button ds-c-button--primary ds-u-margin-top--2">
+  
+               Submit
+               <RightArrow className="ds-u-margin-left--1"/>
+            </button>
+            <br/>
+            {/* <ul className="ds-c-list ds-u-font-size--h5 " >
+               <li><strong>Legislative</strong> – Makes laws (Congress)</li>
+               <li><strong>Executive</strong> – Carries out laws (President, Vice President, Cabinet)</li>
+               <li><strong>Judicial</strong> – Evaluates laws (Supreme Court and other courts)</li>
+            </ul> */}
+         </div>
+      )
+      }
 }
 
 export default Second;
--- src/footer.js
+++ src/footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 function Footer(){
    return(
-   <footer className="App-footer">
-      <a className="ds-c-link--inverse" href="https://www.linkedin.com/in/jesababon/" target="_blank" rel="noopener noreferrer" >
+   <footer className="App-footer ds-u-fill--primary">
+      <a className="ds-c-link" href="https://www.linkedin.com/in/jesababon/" target="_blank" rel="noopener noreferrer" >
         Connect with Jes
       </a>
   </footer>
