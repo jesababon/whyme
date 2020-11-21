33f9551ef254c3ba50f70cd6ce776141cf40823f
Jesica Ababon <jesababon@gmail.com>
2020-11-21 12:51:43 -0500

    onClick display via passed function

--- src/3rdpage.js
+++ src/3rdpage.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React from 'react';
 
+var showing=[];
+var users = ['James', 'Nora', 'Matthew', 'Joe', 'Susan'];
 
 class Third extends React.Component{
-
-   render(){
-      return(
-         <div>
-            <p>
-               Test.
-            </p>
-         </div>
-      )
+   constructor(props){
+      super(props);
+      this.state = {showing};
    }
+   
+   addToShow = () =>{
+      let addition= users[0]
+      this.setState(() => showing=addition)
+      users.shift();
+      console.log(addition)
+   }
+  
+  render(){
+      return (
+         <ul>
 
+               <p>
+               <button onClick={this.addToShow}>
+               Press
+               </button>
+               {showing}
+            </p>
+         </ul>
+      );
+   }  
 }
 
 export default Third;
